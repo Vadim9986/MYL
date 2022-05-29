d13defa85ac4b12998cc576239a714f0f3b1895c
Matvey <kolossmatveyka@gmail.com>
2022-05-29 22:15:35 +0300

    fix

--- MYL/Controllers/SearchLoveController.cs
+++ MYL/Controllers/SearchLoveController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MYL.DataBase;
+using MYL.Models;
 using MYL.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -27,17 +28,26 @@ namespace MYL.Controllers
         [HttpPost]
         public ViewResult GetQuestionary(SearchViewModel search)
         {
-           var model = _db.People.Include(x => x.User).Where(x => x.Age >= search.AgeStart && x.Age<=search.AgeEnd);  
-            model = model.Where(x => x.Gen.Contains(search.Gender));
-            model = model.Where(x => x.Country.Contains(search.Country));
-            switch (search.Sort)
+           List<Questionary> model = _db.People.Include(x => x.User).Where(x => x.Age >= search.AgeStart && x.Age<=search.AgeEnd).ToList();  
+            if(search.Gender is not null)
             {
-                
-                
-              
+                model = model.Where(x => x.Gen.Contains(search.Gender)).ToList();
             }
 
+            if(search.Country is not null)
+            {
+                model = model.Where(x => x.Country.Contains(search.Country)).ToList();
+            }
 
+            switch (search.Sort)
+            {
+                case "NewDesc":
+                    model = model.OrderByDescending(x => x.Id).ToList();
+                    break;
+                case "NewAsc":
+                    model = model.OrderBy(x => x.Id).ToList();
+                    break;
+            }
             return View(model);
         }
         
