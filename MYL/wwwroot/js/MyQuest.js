76f80fd7c0694f36a6223df8d1ee12556f2d91ed
Krylov Danil <danuha2350@gmail.com>
2022-05-31 00:01:07 +0300

    vadym

--- MYL/wwwroot/js/MyQuest.js
+++ MYL/wwwroot/js/MyQuest.js
@@ -1,5 +1,9 @@
 ﻿$('.block_item').click(function (event) {
-    $('.block_item').removeClass('_active__photo')
-    $(this).toggleClass('_active__photo')
-    console.log(event);
+    if ($(this).hasClass('_active__photo')) {
+        $('.block_item').removeClass('_active__photo')
+    }
+    else {
+        $('.block_item').removeClass('_active__photo')
+        $(this).addClass('_active__photo')
+    }
 });
