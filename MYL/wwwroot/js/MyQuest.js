1a9eb162b5d0b73bf5410fa757cfc9c4209bf79e
Matvey <kolossmatveyka@gmail.com>
2022-06-02 20:18:08 +0300

    fix

--- MYL/wwwroot/js/MyQuest.js
+++ MYL/wwwroot/js/MyQuest.js
@@ -1,9 +1,15 @@
-﻿$('.block_item').click(function (event) {
-    if ($(this).hasClass('_active__photo')) {
-        $('.block_item').removeClass('_active__photo')
-    }
-    else {
-        $('.block_item').removeClass('_active__photo')
-        $(this).addClass('_active__photo')
-    }
+﻿// $('.block_item').click(function (event) {
+//     if ($(this).hasClass('_active__photo')) {
+//         $('.block_item').removeClass('_active__photo')
+//     }
+//     else {
+//         $('.block_item').removeClass('_active__photo')
+//         $(this).addClass('_active__photo')
+//     }
+// });
+
+$('.block_item, .view__photo').on('click', function () {
+    //$('body').toggleClass('_lock_scroll')
+    $('.view__photo').toggleClass('_active__view__photo')
+    $('.view__photo__content img').attr('src', $(this).children('img').attr('src'))
 });
