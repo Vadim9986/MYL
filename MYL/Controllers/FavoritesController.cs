7c5e17afd2eb774ae3da260dee8822a6c7b9bc2a
Matvey <kolossmatveyka@gmail.com>
2022-06-06 20:00:40 +0300

    fix

--- MYL/Controllers/FavoritesController.cs
+++ MYL/Controllers/FavoritesController.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MYL.Interfaces;
 
 namespace MYL.Controllers
 {
     public class FavoritesController : Controller
     {
+        private readonly IFavoriteService _favoriteService;
+        public FavoritesController(IFavoriteService favoriteService)
+        {
+            _favoriteService = favoriteService;
+        }
         public IActionResult Favorites()
         {
             var userName = ControllerContext.HttpContext.Session.GetString("Name");
             ViewBag.Account = userName;
-            return View();
+            return View(_favoriteService.Get(userName));
         }
     }
 }
